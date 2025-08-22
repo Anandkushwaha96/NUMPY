df3c3ecd0e87dc4df150f82291cc459a4ec88e25
Anandkushwaha96 <herokuacc5@gmail.com>
2025-08-22 22:57:35 +0530

    update

---
+++ CREATINGARRAY/array.py
@@ -0,0 +1,12 @@
+# 1D Array Creation Example
+"""
+import numpy as np
+
+anand=np.array([9,0,5,6,6])
+print(anand)
+"""
+
+# 2D ARRAY Creation Example 
+import numpy as np
+anand2=np.array([[9,5,6,7,9,0],[9,5,7,8,3,2]])
+print(anand2)
