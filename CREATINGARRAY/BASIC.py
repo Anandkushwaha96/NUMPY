4cdffa8d39ad25d9ee800548c4b1778e65867f2c
Anandkushwaha96 <herokuacc5@gmail.com>
2025-09-03 00:08:21 +0530

    update

---
+++ CREATINGARRAY/BASIC.py
@@ -0,0 +1,97 @@
+# 1D Array Creation Example
+"""
+import numpy as np
+
+anand=np.array([9,0,5,6,6])
+print(anand)
+"""
+
+# 2D ARRAY Creation Example 
+
+"""import numpy as np
+anand2=np.array([[9,5,6,7,9,0],[9,5,7,8,3,2]])
+print(anand2)
+
+"""
+
+# 6D Array Creation Example
+"""
+import numpy as np
+anand3=np.array([[[[[[23,45,2,12],[90,91,92,93],[30,31,32,33]]]]]])
+print(anand3)
+print("Dimension of Array:", np.ndim(anand3))
+print("shape of Array:", anand3.shape)
+print("size of array",anand3.size)
+print("item size of array",anand3.itemsize)
+print("data type of array",anand3.dtype)
+print("nbytes of array",anand3.nbytes)
+print("number of axis",anand3.ndim)
+print("number of elements along axis 0",anand3[0])
+print("number of elements along axis 1",anand3.shape[1])
+print("number of elements along axis 2",anand3.shape[2])
+print("number of elements along axis 3",anand3.shape[3])
+print("number of elements along axis 4",anand3.shape[4])
+print("number of elements along axis 5",anand3.shape[5])
+
+print("number of elements along axis -1",anand3.shape[-1])
+print("number of elements along axis -2",anand3.shape[-2])
+print("number of elements along axis -3",anand3.shape[-3])
+print("number of elements along axis -4",anand3.shape[-4])
+print("number of elements along axis -5",anand3.shape[-5])
+print("number of elements along axis -6",anand3.shape[-6])
+print("total number of elements",anand3.size)
+
+"""
+
+
+
+# ARANGE Function Example
+
+"""
+import numpy as np
+anand4=np.arange(0 ,11,3)
+print(anand4)
+print(type(anand4))
+print("Dimension of Array:", np.ndim(anand4))
+print("shape of Array:", anand4.shape)
+print("size of array",anand4.size)
+print("item size of array",anand4.itemsize)
+print("data type of array",anand4.dtype)
+print("nbytes of array",anand4.nbytes)
+print("number of axis",anand4.ndim)
+"""
+
+
+
+
+# LINSPACE Function Example"
+"""
+import numpy as np
+anand5=np.linspace(0,10,3)
+print(anand5)
+print(type(anand5))
+print("Dimension of array:", np.ndim(anand5))
+print("shape of array", anand5.shape)
+print("size of array",anand5.size)
+print("item size of array",anand5.itemsize)
+print("data type of array",anand5.dtype)
+print("nbytes of array",anand5.nbytes)
+print("number of axis",anand5.ndim)
+
+"""
+# ZEROS Function Example
+
+"""
+import numpy as np 
+anand6=np.zeros((4,3))
+print(anand6)
+print("shpae of array",anand6.shape)
+print("size of array:", anand6.size)
+print(type(anand6))
+print("Dimesion pf array :", np.ndim(anand6))
+print("item size of array",anand6.itemsize)
+print("data type of array",anand6.dtype)
+print("nbytes of array",anand6.nbytes)
+print("number of axis",anand6.ndim)
+"""
+
