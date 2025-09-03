9c83600968cca451e585697727b40f8170dce85e
Anandkushwaha96 <herokuacc5@gmail.com>
2025-09-03 17:36:34 +0530

    update

--- CREATINGARRAY/BASIC.py
+++ CREATINGARRAY/BASIC.py
@@ -95,3 +95,103 @@ print("nbytes of array",anand6.nbytes)
 print("number of axis",anand6.ndim)
 """
 
+# FULL Function Example
+
+"""
+import numpy as np
+anand7=np.full((4,3),5)
+print(anand7)
+print("Dimension of array:",np.ndim(anand7))
+print("size of array:", anand7.size)
+print("size of array:", np.size)
+print("shape of array",anand7.shape)
+print("shape of array",np.shape(anand7))
+print(type(anand7))
+print("item size of array:", anand7.itemsize)
+print("byte in array:", anand7.nbytes)
+
+"""
+
+# ONES Function Example
+
+"""
+import numpy as np 
+anand8=np.ones((3,4))
+print(anand8)
+print("Dimension of array:",np.ndim(anand8))
+print("size of array:", anand8.size)
+print("size of array:", np.size(anand8))
+print("shape of array",anand8.shape)
+print("shape of array",np.shape(anand8))
+print(type(anand8))
+print("item size of array:", anand8.itemsize)
+print("byte in array:", anand8.nbytes)
+"""
+
+# IDENTITY Function Example
+"""
+import numpy as np
+anand9=np.eye(3,4)
+print(anand9)
+print("Dimension of array:",np.ndim(anand9))
+print("size of array:", anand9.size)
+print("size of array:", np.size(anand9))
+print("shape of array",anand9.shape)
+print("shape of array",np.shape(anand9))
+print(type(anand9))
+print("item size of array:", anand9.itemsize)
+print("byte in array:", anand9.nbytes)
+"""
+
+# RANDOM Function For float  Example
+"""
+
+import numpy as np
+anand10=np.random.rand(3,4)
+print(anand10)
+print("Dimension of array:",np.ndim(anand10))
+print("size of array:", anand10.size)
+print("size of array:", np.size(anand10))
+print("shape of array",anand10.shape)
+print("shape of array",np.shape(anand10))
+print(type(anand10))
+print("item size of array:", anand10.itemsize)
+print("byte in array:", anand10.nbytes)
+
+"""
+
+
+# RANDOM Function For Integer Example
+
+"""
+import numpy as np
+anand11=np.random.randint(1,10,(3,4))
+print(anand11)
+print("Dimension of array:",np.ndim(anand11))
+print("size of array:", anand11.size)
+print("size of array:", np.size(anand11))
+print("shape of array",anand11.shape)
+print("shape of array",np.shape(anand11))
+print(type(anand11))
+print("item size of array:", anand11.itemsize)
+print("byte in array:", anand11.nbytes)
+
+"""
+# astype Function Example
+
+
+"""
+import numpy as np
+anand12=np.array([1,2,3,4,5])
+anand12=anand12.astype('int64')
+print(anand12)
+print("Dimension of array:",np.ndim(anand12))
+print("size of array:", anand12.size)
+print("size of array:", np.size(anand12))
+print("shape of array",anand12.shape)
+print("shape of array",np.shape(anand12))
+print(type(anand12))
+print("item size of array:", anand12.itemsize)
+print("byte in array:", anand12.nbytes)
+print("data type of array",anand12.dtype)
+"""
